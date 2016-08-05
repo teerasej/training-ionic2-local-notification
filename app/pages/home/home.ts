d12dc095ef2d558d81306cb608217a2972e5606b
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-08-05 21:30:35 +0700

    Init

---
+++ app/pages/home/home.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {NavController, Alert} from 'ionic-angular';
+import {LocalNotifications} from 'ionic-native';
+
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  showAlert() {
+    LocalNotifications.schedule({
+      id: 1,
+      text: "Single Notification"
+    });
+  }
+}
