8e3b0a21a3aee11b8853c1ab33cda9d7a2f475ea
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-08-07 21:47:13 +0700

    Play selected sound file when notification happenned

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, Alert} from 'ionic-angular';
 import {LocalNotifications} from 'ionic-native';
-
+import { Device } from 'ionic-native';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -12,12 +12,22 @@ export class HomePage {
   }
 
   showAlert() {
+
+    let soundFilePath = 'file://';
+
+    if(Device.device.platform == 'Android'){
+      soundFilePath += 'sound.mp3';
+    } else {
+      soundFilePath += 'sound.caf';
+    }
+
     LocalNotifications.schedule({
       id: 1,
       text: "Single Notification",
       // Badge number for iOS
       // Notification number for Android
-      badge: 3
+      badge: 3,
+      sound: soundFilePath
     });
   }
 }
